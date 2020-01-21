69f7ded66457a34f96aca86c3eecfe89abf04b06
Owen Ridgeway <owen.ridgeway@gmail.com>
2020-01-21 17:01:58 -0600

    Added code to drive subsystem

--- src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -7,18 +7,62 @@
 
 package frc.robot.subsystems;
 
+import edu.wpi.first.wpilibj.Spark;
+import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class ExampleSubsystem extends SubsystemBase {
-  /**
-   * Creates a new ExampleSubsystem.
-   */
-  public ExampleSubsystem() {
+public class DriveSubsystem extends Subsystem {
+	// Put methods for controlling this subsystem
+	// here. Call these from Commands.
+	
+	private final Spark leftDrive = RobotMap.leftDriveMotorController;
+	private final Spark rightDrive = RobotMap.rightDriveMotorController;
+	private final DifferentialDrive differentialDrive1 = RobotMap.drivetrain;
 
-  }
+	private final ADIS16448_IMU imu = RobotMap.imu;
 
-  @Override
-  public void periodic() {
-    // This method will be called once per scheduler run
-  }
+	public DriveSubsystem() {
+		addChild("Left CIM", (Spark) leftDrive);
+		addChild("Right CIM", (Spark) rightDrive);
+		addChild("IMU", imu);
+		addChild("Left Encoder",RobotMap.leftEncoder);
+		addChild("Right Encoder",RobotMap.rightEncoder);
+		differentialDrive1.setSafetyEnabled(false);
+		differentialDrive1.setExpiration(0.1);
+		differentialDrive1.setMaxOutput(1.0);
+	}
+
+	public void initDefaultCommand() {
+		// Set the default command for a subsystem here.
+		setDefaultCommand(new DriveCommand());
+	}
+
+	public void tankDrive(double leftSpeed, double rightSpeed) {
+		differentialDrive1.tankDrive(leftSpeed, rightSpeed);
+	}
+
+	public void arcadeDrive(double forwardSpeed, double rotationSpeed) {
+		differentialDrive1.arcadeDrive(forwardSpeed, rotationSpeed);
+	}
+
+	public void log() {
+		SmartDashboard.putNumber("Gyro-Angle", imu.getAngle());
+		SmartDashboard.putNumber("Gyro-X", imu.getAngleX());
+		SmartDashboard.putNumber("Gyro-Y", imu.getAngleY());
+		SmartDashboard.putNumber("Gyro-Z", imu.getAngleZ());
+
+		SmartDashboard.putNumber("Pressure: ", imu.getBarometricPressure());
+		SmartDashboard.putNumber("Temperature: ", imu.getTemperature());
+		
+//		SmartDashboard.putNumber("Left Sonar Distance: ", RobotMap.leftSonar.getValue());
+//		SmartDashboard.putNumber("Right Sonar Distance: ", RobotMap.rightSonar.getValue());
+		
+		SmartDashboard.putNumber("Left Speed", leftDrive.get());
+		SmartDashboard.putNumber("Right Speed", rightDrive.get());
+		
+		SmartDashboard.putNumber("Left Encoder", RobotMap.leftEncoder.get());
+		SmartDashboard.putNumber("Right Encoder", RobotMap.rightEncoder.get());
+	}
 }
