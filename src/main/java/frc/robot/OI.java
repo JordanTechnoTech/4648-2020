bd9a96a87f696570cb802e64dfb1ed3960b8b8a4
Aaron Backlund <35507969+abacklund2003@users.noreply.github.com>
2020-02-08 14:34:57 -0600

    Moved OI to RobotMap
    Moved OI to RobotMap

--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -7,12 +7,6 @@ import frc.robot.command.ShootCommand;
 
 public class OI {
 
-    public final TechnoTechXBoxController controller0 = new TechnoTechXBoxController(0);
 
-    public OI() {
-        controller0.xButton.whenPressed(new ColorSensorCommand());
-        controller0.aButton.whenActive(new BallStorageCommand());
-        controller0.bButton.whenPressed(new ShootCommand());
-        controller0.lbButton.toggleWhenPressed(new IntakeCommand());
-    }
+    
 }
---
+++ src/main/java/frc/robot/command/Target.java
@@ -0,0 +1,9 @@
+package frc.robot.command;
+
+public class Target {
+    private double height; 
+    public double getHeight(){
+        return this.height;
+    }
+
+}
