35c873c739b7005ab6395bbb50f38c7e2a3fc976
IsaacMcLeod24 <59515911+IsaacMcLeod24@users.noreply.github.com>
2020-02-15 17:12:57 -0600

    finished limelight code
    finished limelight code

---
+++ src/main/java/frc/robot/command/RangeValue.java
@@ -0,0 +1,13 @@
+package frc.robot.command;
+
+public class RangeValue {
+    public double startValue;
+    public double endValue;
+    public double result;
+
+    public RangeValue(double startValue, double endValue, double result) {
+        this.startValue = startValue;
+        this.endValue = endValue;
+        this.result = result;
+    }
+}
